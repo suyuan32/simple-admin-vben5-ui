e777a4d9ab5df97068aa51b2f0b65873af348174
Ryan Su <yuansu.china.work@gmail.com>
2024-11-08 08:15:03 +0800

    fix: adjust api request codes

---
+++ apps/simple-admin-core/src/api/fms/cloudFile.ts
@@ -0,0 +1,74 @@
+import {
+  type BaseDataResp,
+  type BaseListReq,
+  type BaseResp,
+  type BaseUUIDReq,
+  type BaseUUIDsReq,
+} from '#/api/model/baseModel';
+import { requestClient } from '#/api/request';
+
+import {
+  type CloudFileInfo,
+  type CloudFileListResp,
+} from './model/cloudFileModel';
+
+enum Api {
+  CreateCloudFile = '/fms-api/cloud_file/create',
+  DeleteCloudFile = '/fms-api/cloud_file/delete',
+  GetCloudFileById = '/fms-api/cloud_file',
+  GetCloudFileList = '/fms-api/cloud_file/list',
+  UpdateCloudFile = '/fms-api/cloud_file/update',
+  uploadFile = '/fms-api/cloud_file/upload',
+}
+
+/**
+ * @description: Get cloud file list
+ */
+
+export const getCloudFileList = (params: BaseListReq) => {
+  return requestClient.post<BaseDataResp<CloudFileListResp>>(
+    Api.GetCloudFileList,
+    params,
+  );
+};
+
+/**
+ *  @description: Create a new cloud file
+ */
+export const createCloudFile = (params: CloudFileInfo) => {
+  return requestClient.post<BaseResp>(Api.CreateCloudFile, params);
+};
+
+/**
+ *  @description: Update the cloud file
+ */
+export const updateCloudFile = (params: CloudFileInfo) => {
+  return requestClient.post<BaseResp>(Api.UpdateCloudFile, params);
+};
+
+/**
+ *  @description: Delete cloud files
+ */
+export const deleteCloudFile = (params: BaseUUIDsReq) => {
+  return requestClient.post<BaseResp>(Api.DeleteCloudFile, params);
+};
+
+/**
+ *  @description: Get cloud file By ID
+ */
+export const getCloudFileById = (params: BaseUUIDReq) => {
+  return requestClient.post<BaseDataResp<CloudFileInfo>>(
+    Api.GetCloudFileById,
+    params,
+  );
+};
+
+/**
+ * @description: Upload interface
+ */
+// export function uploadApi(
+//   params: UploadFileParams,
+//   onUploadProgress: (progressEvent: AxiosProgressEvent) => void,
+// ) {
+//   return requestClient.upload(Api.uploadFile, params);
+// }
---
+++ apps/simple-admin-core/src/api/fms/file.ts
@@ -0,0 +1,75 @@
+import { requestClient } from '#/api/request';
+
+import {
+  type BaseDataResp,
+  type BaseListReq,
+  type BaseResp,
+  type BaseUUIDsReq,
+} from '../model/baseModel';
+import { type FileListResp, type updateFileInfoReq } from './model/fileModel';
+// import { type UploadApiResp } from '@/api/sys/model/uploadModel';
+// import { type AxiosProgressEvent } from 'axios';
+
+enum Api {
+  DeleteFile = '/fms-api/file/delete',
+  DownloadFile = '/fms-api/file/download',
+  GetFileList = '/fms-api/file/list',
+  SetFileStatus = '/fms-api/file/status',
+  UpdateFileInfo = '/fms-api/file/update',
+  uploadFile = '/fms-api/upload',
+}
+
+/**
+ * @description: Upload interface
+ */
+// export function uploadApi(params: UploadFileParams, onUploadProgress: (progressEvent: AxiosProgressEvent) => void) {
+//   return requestClient.uploadFile<BaseDataResp<UploadApiResp>>({
+//     url: Api.uploadFile,
+//     onUploadProgress
+//   }, params);
+// }
+
+/**
+ * @description: Get file list
+ */
+
+export const getFileList = (params: BaseListReq) => {
+  return requestClient.post<BaseDataResp<FileListResp>>(
+    Api.GetFileList,
+    params,
+  );
+};
+
+/**
+ *  author: ryan
+ *  @description: update file info
+ */
+export const UpdateFileInfo = (params: updateFileInfoReq) => {
+  return requestClient.post<BaseResp>(Api.UpdateFileInfo, params);
+};
+
+/**
+ *  author: Ryan Su
+ *  @description: delete files
+ */
+export const deleteFile = (params: BaseUUIDsReq) => {
+  return requestClient.post<BaseResp>(Api.DeleteFile, params);
+};
+
+/**
+ *  author: Ryan Su
+ *  @description: set file's status
+ */
+export const setFileStatus = (id: string, status: number) =>
+  requestClient.post(Api.SetFileStatus, { id, status });
+
+/**
+ *  author: Ryan Su
+ *  @description: download file
+ */
+
+// export const downloadFile = (id: number) =>
+//   requestClient.get({
+//     url: Api.DownloadFile + '/' + id,
+//     responseType: 'blob',
+//   });
