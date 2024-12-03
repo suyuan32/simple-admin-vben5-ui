suyuan32/simple-admin-vben5-ui

commit 00844e8a4c5b0bc990c034e49fb7ee62115847a2

fix: optimize image upload emit event

apps/simple-admin-core/src/components/form/ImageUpload.vue
@@ -14,6 +14,7 @@ import {
 import { $t } from '@vben/locales';
 
 import { PlusOutlined } from '@ant-design/icons-vue';
+import { useVModel } from '@vueuse/core';
 import { message, Modal, Upload } from 'ant-design-vue';
 import { type UploadRequestOption } from 'ant-design-vue/lib/vc-upload/interface';
 import { isArray, isObjectType, isString } from 'remeda';
@@ -86,7 +87,11 @@ const props = defineProps({
   },
 });
 
-const emit = defineEmits(['change', 'update:value', 'delete']);
+const emits = defineEmits(['update:value']);
+const state = useVModel(props, 'value', emits, {
+  defaultValue: props.value,
+  passive: true,
+});
 
 enum UploadResultStatus {
   // eslint-disable-next-line no-unused-vars
@@ -178,7 +183,7 @@ function isImgTypeByName(name: string) {
 }
 
 watch(
-  () => props.value,
+  () => state.value,
   (v) => {
     if (isInnerOperate.value) {
       isInnerOperate.value = false;
@@ -251,10 +256,11 @@ const handleRemove = async (file: UploadFile) => {
   if (fileList.value) {
     const index = fileList.value.findIndex((item) => item.uid === file.uid);
     index !== -1 && fileList.value.splice(index, 1);
-    const value = getValue();
     isInnerOperate.value = true;
-    emit('change', value);
-    emit('delete', file);
+    emits(
+      'update:value',
+      fileList.value.map((item) => item.url),
+    );
   }
 };
 
@@ -296,8 +302,7 @@ async function customRequest(info: UploadRequestOption<any>) {
     info.onSuccess!(result.data);
     const value = getValue();
     isInnerOperate.value = true;
-    emit('change', value);
-    emit('update:value', value);
+    emits('update:value', value);
   } catch (error: any) {
     info.onError!(error);
   }

commit 45d3435e5d70b011daf8e579ea9a50b5e63193ef

fix: bug of uploading avatar
caused by:
https://github.com/suyuan32/simple-admin-vben5-ui/commit/00844e8a4c5b0bc990c034e49fb7ee62115847a2

apps/simple-admin-core/src/components/form/ImageUpload.vue
@@ -87,7 +87,7 @@ const props = defineProps({
   },
 });
 
-const emits = defineEmits(['update:value']);
+const emits = defineEmits(['change', 'update:value', 'delete']);
 const state = useVModel(props, 'value', emits, {
   defaultValue: props.value,
   passive: true,
@@ -256,11 +256,9 @@ const handleRemove = async (file: UploadFile) => {
   if (fileList.value) {
     const index = fileList.value.findIndex((item) => item.uid === file.uid);
     index !== -1 && fileList.value.splice(index, 1);
+    const value = getValue();
     isInnerOperate.value = true;
-    emits(
-      'update:value',
-      fileList.value.map((item) => item.url),
-    );
+    emits('update:value', value);
   }
 };
 